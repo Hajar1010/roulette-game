7bcbfdfe75b51189c7bb9de7188e00cc7fe72e28
Hajar <hajaressalihi9@gmail.com>
2025-07-19 13:46:46 +0100

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -1,44 +1,44 @@
 import random
 from math import*
-#Configurer la Roue de Roulette
+# Configure the Roulette Wheel
 list = [i for i in range (0,37)]
-#Gérer le Budget du Joueur
+# Manage the Player's Budget
 
-Budget_Initial=int(input("votre budget : "))
-print("votre Budget est : ",Budget_Initial )
-print("votre objectif est de maximiser votre gains avant de décider d'arreter !!!!")
+Budget_Initial=int(input("Your budget: "))
+print("Your budget is: ",Budget_Initial )
+print("Your goal is to maximize your winnings before deciding to stop!!!!")
 
 while Budget_Initial>0:
-    # Accepter les Paris
-    num = int(input("Choisir un numéro entre 0 et 36 sur lequel vous souhaitez parier: "))
+    # Accept Bets
+    num = int(input("Choose a number between 0 and 36 you wish to bet on: "))
     while num not in list:
-        print("Le numéro doit être entre 0 et 36.")
-        num = int(input("Choisir un numéro entre 0 et 36 sur lequel vous souhaitez parier: "))
+        print("The number must be between 0 and 36.")
+        num = int(input("Choose a number between 0 and 36 you wish to bet on: "))
 
-    argent = int(input("Combien d'argent souhaite vous miser sur ce numéro "))
+    argent = int(input("How much money do you want to bet on this number? "))
     # Check the condition
     if argent <= Budget_Initial:
-        print("Le budget est suffisant!")
+        print("The budget is sufficient!")
     else:
-        argent = int(input("encore une fois Combien d'argent souhaite vous miser sur ce numéro  il doit etre moins ou egale a vous budget"))
+        argent = int(input("Again, how much money do you want to bet on this number? It must be less than or equal to your budget."))
 
-    # FaireTouener la Roue de Roulette
+    # Spin the Roulette Wheel
     numé = random.choice(list)  # random.choice(list)
-    print("le numéro tiré c'est : ", numé)
-    # Logique de Gain et Perte
+    print("The drawn number is: ", numé)
+    # Win and Loss Logic
     if numé == num:
         Budget_Initial = argent * 35
-        print("a winnn !! ,votre nouveau budget c'est : ", Budget_Initial)
+        print("A win!! Your new budget is: ", Budget_Initial)
     else:
         Budget_Initial = Budget_Initial - argent
-        print("youu lose !! , votre nouveau budget c'est : ", Budget_Initial)
-    # Terminer le jeu
-    reponse=input("Est ce que vous souhaite continuer a jouer entrer oui ou non ")
+        print("You lose!! Your new budget is: ", Budget_Initial)
+    # End the game
+    reponse=input("Do you want to continue playing? Enter yes or no: ")
     if reponse =='oui':
         continue
     if reponse=='non':
-        print(f"Merci d’avoir joué ! Votre budget final est {Budget_Initial} . À bientôt !")
+        print(f"Thank you for playing! Your final budget is {Budget_Initial}. See you soon!")
         break
 else:
-    print("vous n'avais plus d'argent pour continuer")
-print("nice game À bientôt !")
+    print("You no longer have money to continue.")
+print("Nice game. See you soon!")
